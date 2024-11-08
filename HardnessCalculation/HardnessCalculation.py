5cb794d67009a33529dd1c7e18d251af377e9f38
prakapniou <prakapniou@gmail.com>
2024-11-08 18:51:36 +0300

    Add maon window

---
+++ HardnessCalculation/HardnessCalculation.py
@@ -0,0 +1,12 @@
+from tkinter import *
+ 
+root = Tk()
+root.title("Hardness calculation")
+root.geometry("300x250+200+200")
+icon = PhotoImage(file = "icon.png")
+root.iconphoto(False, icon)
+ 
+#label = Label(text="Tehnolit")
+#label.pack()
+ 
+root.mainloop()
